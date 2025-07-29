a15e7cea74da7e32ffb7babe7766a5d24c86a6ca
AryanRogye <161079183+AryanRogye@users.noreply.github.com>
2025-07-28 22:30:11 -0500

    added a easy way to make the screen show up to whichever screen the mouse is on

--- ComfyTab/Views/Overlay/Overlay.swift
+++ ComfyTab/Views/Overlay/Overlay.swift
@@ -40,6 +40,7 @@ class Overlay: ObservableObject {
         }
         
         if !overlay.isVisible {
+            calculateNewScreenPosition()
             NSApp.activate(ignoringOtherApps: true)
             overlay.makeKeyAndOrderFront(nil)
         }
@@ -53,17 +54,19 @@ class Overlay: ObservableObject {
         }
     }
     
+    // MARK: - Calculate New Show
+    
+    /// Function will use a new NSScreen.main becuase the user may have a different screen in use
+    private func calculateNewScreenPosition() {
+        guard let screen = getScreenUnderMouse() else { return }
+        overlay.setFrame(centerRect(on: screen), display: true, animate: false)
+    }
+    
     // MARK: - Prepare Overlay
     private func prepareOverlay() {
-        guard let screen = NSScreen.main else { return }
-        let screenFrame = screen.frame
+        guard let screen = getScreenUnderMouse() else { return }
         
-        let panelRect = NSRect(
-            x: (screenFrame.width - overlayWidth) / 2,
-            y: (screenFrame.height - overlayHeight) / 2,
-            width: overlayWidth,
-            height: overlayHeight
-        )
+        let panelRect = centerRect(on: screen)
         
         overlay = FocusablePanel(
             contentRect: panelRect,
@@ -98,5 +101,23 @@ class Overlay: ObservableObject {
         overlay.contentView = hostingView
         
         self.hide()
+    }
+}
+
+/// Quick Mouse Functions
+extension Overlay {
+    private func getScreenUnderMouse() -> NSScreen? {
+        let mouseLocation = NSEvent.mouseLocation
+        return NSScreen.screens.first(where: { $0.frame.contains(mouseLocation) })
+    }
+    
+    private func centerRect(on screen: NSScreen) -> NSRect {
+        let frame = screen.frame
+        return NSRect(
+            x: frame.origin.x + (frame.width - overlayWidth) / 2,
+            y: frame.origin.y + (frame.height - overlayHeight) / 2,
+            width: overlayWidth,
+            height: overlayHeight
+        )
     }
 }
